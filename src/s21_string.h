8037d235bfef647fb97571eb60e59586ac466486
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2023-01-13 15:06:33 +0300

    copied code from old pool project

--- src/s21_string.h
+++ src/s21_string.h
@@ -1,4 +1,16 @@
 #ifndef C2_S21_STRINGPLUS_0_S21_STRING_H
 #define C2_S21_STRINGPLUS_0_S21_STRING_H
 
+#define NULL ((void *)0)
+
+typedef unsigned long size_t;
+
+size_t s21_strlen(const char* str);
+int s21_strcmp(const char* first, const char* second);
+char* s21_strcpy(char* restrict destination, const char *restrict source);
+char* s21_strcat(char *restrict destination, const char *restrict source);
+char* s21_strchr(const char* src, int sym);
+char* s21_strstr(const char* haystack, const char* needle);
+char* s21_strtok(char* restrict str, const char* restrict delim);
+
 #endif  // C2_S21_STRINGPLUS_0_S21_STRING_H
