92888e0abec7a8124c312fdf13b0aa19a0ce03ce
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2023-01-24 18:09:56 +0300

    reader parser implemented

--- src/s21_format_functions.c
+++ src/s21_format_functions.c
@@ -143,9 +143,35 @@ void init_reader(ReaderFormat* reader) {
   reader->parsed_length = 0;
 }
 
-// void parse_into_reader(ReaderFormat* reader, const char* src) {
-//   // TODO: should be implemented with the same logic as parse_into_writer()
-// }
+void check_reader_asterisk(ReaderFormat* reader, const char* src) {
+  if (*src == '*') {
+    reader->skip_assignment = 1;
+    reader->parsed_length += 1;
+  }
+}
+
+void parse_into_reader(ReaderFormat* reader, const char* src) {
+  // width
+  check_reader_asterisk(reader, src + reader->parsed_length);
+  int width = str_to_int(&src[reader->parsed_length], &reader->parsed_length);
+  if (width != -1) {
+    reader->width = width;
+  }
+
+  // length
+  check_reader_asterisk(reader, src + reader->parsed_length);
+  if (s21_strchr(lengths, src[reader->parsed_length]) != NULL) {
+    reader->length = src[reader->parsed_length];
+    ++reader->parsed_length;
+  }
+
+  // specification
+  check_reader_asterisk(reader, src + reader->parsed_length);
+  if (s21_strchr(specifications, src[reader->parsed_length]) != NULL) {
+    reader->specification = src[reader->parsed_length];
+    ++reader->parsed_length;
+  }
+}
 
 // TODO: vargs or smth like that should be used in these functions
 // TODO: it would be nice to know it
